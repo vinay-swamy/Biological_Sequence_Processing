9d711bfbb4ed511c1d42346e51783b3256af0c00
Vinay Siddhartha Swamy <swamyvinny@gmail.com>
2019-12-09 14:55:11 -0500

    now saving both conditions to a single file with tuples of kmer list + label, and generating lineSentence format output for fast doc2vec implementation

--- scripts/kmerize_fasta.py
+++ scripts/kmerize_fasta.py
@@ -4,27 +4,46 @@ from Bio import SeqIO
 from os import path 
 from pickle import dump
 import sys 
-def kmerize_sequence(seq,k, n=1):
+
+
+
+def kmerize_sequence(seq,k, n, label):
     '''
     k is the length of the kmer, and n is the distance between each kmer
     '''
     res = [seq[i:(i+k)] for i in range(0, len(seq)-k+1, n)]
-    return(res)
+    
+    return((res, label))
+
+
+def process_fasta(file, k, n, label):
+    fpath = path.expanduser(file) if '~' in file else path.abspath(file)
+    fasta = SeqIO.parse(fpath, format='fasta')
+    kmer_list = [kmerize_sequence(
+        str(record.seq), k, n,label) for record in fasta]
+    return(kmer_list)
+
+
+
+
 
 #%%
 
-fasta=sys.argv[1]
-kmer_size=int(sys.argv[2])# k
-kmer_dist=int(sys.argv[3])# l
-kmer_outfile=sys.argv[4]
+ref_fasta_file=sys.argv[1]
+dummy_fasta_file=sys.argv[2]
+kmer_size=int(sys.argv[3])# k
+kmer_dist=int(sys.argv[4])# l
+kmer_outfile=sys.argv[5]
+lineSentence_outfile=sys.argv[6]
 
-fasta_path = path.expanduser(fasta) if '~' in fasta else path.abspath(fasta)
-fasta=SeqIO.parse(fasta_path, format='fasta')
-kmer_list=[kmerize_sequence(str(record.seq),kmer_size,kmer_dist) for record in fasta ]
+all_records_kmers = process_fasta(ref_fasta_file, kmer_size, kmer_dist, 1) + process_fasta(dummy_fasta_file, kmer_size, kmer_dist, 0)
+
+
 # %%
 
 with open(kmer_outfile, 'wb+') as ofl:
-    dump(kmer_list, ofl)
-# %%
+    dump(all_records_kmers, ofl)
 
-# %%
+with open(lineSentence_outfile, 'w+') as lso:
+    [lso.write(' '.join(record[0]) + '\n') for record in all_records_kmers]
+
